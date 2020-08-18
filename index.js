9e38a9110a3ea40a2e2e318a68833e70792aab91
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2020-08-17 19:06:21 -0500

    This is way too complicated and confusing

--- index.js
+++ index.js
@@ -2,67 +2,72 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *  Counter1 is returning another counter
+ * 2. Which of the two uses a closure? How can you tell?
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *  If you wanted to access the variable globally you would use counter2. Counter1 would be used to access certain variables.
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
-  return function counter() {
-   return count++;
-  }
+  return function counter1() {
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
 
 // counter2 code
-let count = 0;
+let count = 7;
 
 function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(array){
-  array.sort(() => Math.random() -0.75);
+function inning(array) {
+  array.sort(() => Math.random() - 0.75);
   return array;
 }
 let points = [1, 2, 3, 4, 5];
 randomize(points);
 console.log(randomize(numbers));
+
+//function inning(){
+//  return Math.floor(math.random() * 3);
+//}
 
 /* Task 3: finalScore()
 
@@ -76,14 +81,21 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(cbInning, numInnings) { //cbinning - callback param, num- number param
+  let score ={
+    home = 0,
+    away = 0
+  }
+  for(i = 0; i <= numInnings; i++);
+  score.home += cbInning();
+  score.away += cbInning();
+  }
+  return score;
 }
 
+console.log(finalScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -104,9 +116,5 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
-
-
+//I attemtped to follow along during the stand-up but
+//I am completely lost on this one. 
